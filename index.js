9694a34620a7ed1d530717866dd691d526d01528
robertlek <lecarobertandrei@gmail.com>
2023-04-10 19:49:20 +0300

    Implemented an insert function to add new rows in the table.

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ function getTeamsRequest() {
     });
 }
 
+function createTeamRequest(team) {
+    return fetch("http://localhost:3000/teams-json/create", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(team)
+    })
+    .then((r) => r.json());
+}
+
 function getTeamsAsHTML(team) {
     return `
         <tr>
@@ -17,15 +28,42 @@ function getTeamsAsHTML(team) {
             <td>${team.members}</td>
             <td>${team.name}</td>
             <td>${team.url}</td>
+            <td></td>
         </tr>
     `;
 }
 
 function showTeams(teams) {
     const html = teams.map(getTeamsAsHTML);
-    document.querySelector("table tbody").innerHTML = html.join("");
+    $("table tbody").innerHTML = html.join("");
+}
+
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+function formSubmit(e) {
+    e.preventDefault();
+
+    const team = {
+        promotion: $("#promotion").value,
+        members: $("#members").value,
+        name: $("#project-name").value,
+        url: $("#project-url").value
+    };
+
+    createTeamRequest(team).then((status) => {
+        console.info("status", status);
+        window.location.reload();
+    });
+}
+
+function initEvents() {
+    $("#edit-form").addEventListener("submit", formSubmit);
 }
 
 getTeamsRequest().then((teams) => {
     showTeams(teams);
 });
+
+initEvents();

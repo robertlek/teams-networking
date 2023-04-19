719827d7c0120ae75384d2ab52a60c8a358bc631
robertlek <lecarobertandrei@gmail.com>
2023-04-19 19:29:52 +0300

    Converted searchTeams function to a pure function.

--- src/index.js
+++ src/index.js
@@ -103,15 +103,15 @@ function formSubmit(e) {
     }
 }
 
-function searchTeams(search) {
+function searchTeams(teams, search) {
     search = search.toLowerCase();
-    return allTeams.filter(team => {
-        if (team.members.toLowerCase().includes(search)) {
-            return true;
-        }
-        if (team.promotion.toLowerCase().includes(search)) {
-            return true;
-        }
+    return teams.filter(team => {
+        return (
+            team.members.toLowerCase().includes(search) ||
+            team.name.toLowerCase().includes(search) ||
+            team.promotion.toLowerCase().includes(search) ||
+            team.url.toLowerCase().includes(search)
+        );
     });
 }
 
@@ -124,7 +124,7 @@ function initEvents() {
 
     $("#search").addEventListener("input", e => {
         const search = e.target.value;
-        const teams = searchTeams(search);
+        const teams = searchTeams(allTeams, search);
         showTeams(teams);
     });
 
